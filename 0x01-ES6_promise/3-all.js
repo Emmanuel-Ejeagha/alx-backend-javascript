dd52119e621338be9da9263910a457b47fe5fca1
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-28 23:29:16 +0100

    added: task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const [photoResponse, userResponse] = values;
-      console.log(
-        photoResponse.body,
-        userResponse.data.firstName,
-        userResponse.data.lastName
-      );
-    })
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    const [photoResponse, userResponse] = values;
+    console.log(photoResponse.body, userResponse.data.firstName, userResponse.data.lastName);
+  })
     .catch((error) => {
       console.log('Signup system offline');
     });
