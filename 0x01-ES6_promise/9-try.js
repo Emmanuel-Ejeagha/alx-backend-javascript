5f419539a117aadf6dbea80f9865addd0b96019c
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-28 23:24:01 +0100

    added: task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,9 +2,9 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const data = mathFunction();
-    queue.push(data, 'Guardrial was processwd');
+    queue.push(data, 'Guardrial was processed');
   } catch (error) {
-    queue.push(`Error: ${error.message}`, 'Guaedrail was proceessed');
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
   return queue;
 }
