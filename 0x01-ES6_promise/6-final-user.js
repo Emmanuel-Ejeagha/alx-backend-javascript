8f493dd454af58e1eb72be098510cca1abd291ac
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-05-28 23:30:53 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName).then((value) => value),
-    uploadPhoto(fileName).catch((error) => error),
-  ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([signUpUser(firstName, lastName).then((value) => value),
+    uploadPhoto(fileName).catch((error) => error)]);
 
   return results.map((result) => ({
     status: result.status,
