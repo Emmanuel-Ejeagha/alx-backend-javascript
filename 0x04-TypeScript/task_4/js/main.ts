3102ddc576b54777b7e55b68f148246c9786e7fa
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-05 23:29:45 +0100

    task 4

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,39 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+    firstName: 'Emmanuel',
+    lastName: 'Ejeagha',
+    experienceTeachingC: 10
+}
+
+console.log('C++')
+cpp.setTeacher = {
+    firstName: 'cTeacher',
+    lastName: 'cTeacher'
+};
+console.log(cpp.getRequirements())
+console.log(cpp.getAvailableTeacher())
+
+
+console.log('Java')
+java.setTeacher = {
+    firstName: 'cTeacher',
+    lastName: 'cTeacher'
+};
+console.log(java.getRequirements())
+console.log(java.getAvailableTeacher())
+
+
+console.log('React')
+react.setTeacher = {
+    firstName: 'cTeacher',
+    lastName: 'cTeacher'
+};
+console.log(react.getRequirements())
+console.log(react.getAvailableTeacher())
