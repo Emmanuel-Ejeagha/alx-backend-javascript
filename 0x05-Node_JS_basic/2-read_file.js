ccbf119b800c719d5b1c582daa822f44e1d6b4fa
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-26 14:18:49 +0100

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const results = data.split('\n');
+
+    const fields = {};
+    let count = -1;
+    for (const struct of results) {
+      if (struct !== '') {
+        count += 1;
+        const info = struct.split(',');
+        if (Object.prototype.hasOwnProperty.call(fields, info[3])) {
+          fields[info[3]].push(info[0]);
+        } else {
+          fields[info[3]] = [];
+          fields[info[3]].push(info[0]);
+        }
+      }
+    }
+    delete fields.field;
+    console.log(`Number of students: ${count}`);
+    const keys = Object.keys(fields);
+    for (const dat of keys) {
+      console.log(
+        `Number of students in ${dat}: ${fields[dat].length}. List: ${fields[
+          dat
+        ].join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
