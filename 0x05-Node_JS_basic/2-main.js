0571f55d245d4e8b54253178556b1bc4a5a66613
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-27 00:52:43 +0100

    modified

--- 0x05-Node_JS_basic/2-main.js
+++ 0x05-Node_JS_basic/2-main.js
@@ -1,3 +1,37 @@
-const countStudents = require('./2-read_file');
+const fs = require('fs');
 
-countStudents('nope.csv');
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const results = data.split('\n');
+
+    const fields = {};
+    let count = -1;
+    for (const struct of results) {
+      if (struct !== '') {
+        count += 1;
+        const info = struct.split(',');
+        if (Object.prototype.hasOwnProperty.call(fields, info[3])) {
+          fields[info[3]].push(info[0]);
+        } else {
+          fields[info[3]] = [];
+          fields[info[3]].push(info[0]);
+        }
+      }
+    }
+    delete fields.field;
+    console.log(`Number of students: ${count}`);
+    const keys = Object.keys(fields);
+    for (const dat of keys) {
+      console.log(
+        `Number of students in ${dat}: ${fields[dat].length}. List: ${fields[
+          dat
+        ].join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
