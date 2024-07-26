c519bfe4b23e41bdc624bd0e348ab4db5ad0f3cc
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-26 16:24:44 +0100

    more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,25 @@
+const express = required('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  let [file] = process.argv.slice(2);
+  file = file || '';
+
+  countStundents(file)
+    .then((value) => {
+      res.send(`<pre>This is the list of our students\n${value}</pre>`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245, 'localhost', () => {
+  console.log('server is running');
+});
+module.exports = app;
