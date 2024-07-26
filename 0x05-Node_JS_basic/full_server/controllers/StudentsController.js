360b176b0cf34c1c4dadff98a78b29e7689778ff
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-27 00:25:17 +0100

    StudentsController.js

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    let [file] = process.argv.slice(2);
+    file = file || 'nofile';
+
+    readDatabase(file)
+      .then((val) => {
+        let info = 'This is the list of our students\n';
+        const objK = Object.keys(val);
+        for (const key of objK) {
+          info += `Number of students in ${key}: ${
+            val[key].length
+          }. List: ${val[key].join(', ')}\n`;
+        }
+        response.status(200).send(`<pre>${info}</pre>`);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const mj = request.params.major;
+
+    if (mj === 'CS' || mj === 'SWE') {
+      let [file] = process.argv.slice(2);
+      file = file || 'nofile';
+
+      readDatabase(file).then((val) => {
+        const info = `List: ${val[mj].join(', ')}`;
+        response.status(200).send('Cannot load the database');
+      });
+    } else {
+      response.status(500).send('Major parameter must be CS or SWE');
+    }
+  }
+}
+
+export default StudentsController;
