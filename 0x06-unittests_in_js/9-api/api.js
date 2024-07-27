ccbbb55ecd884a780b3d60e85e3f1874f63c19d5
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-27 14:31:39 +0100

    9-api/

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,16 +1,11 @@
 const express = require('express');
-
 const app = express();
+app.listen(7865, () => console.log('API available on localhost port 7865'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = req.params.id;
-  res.send(`Payment methods for cart ${id}`);
-});
-
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
