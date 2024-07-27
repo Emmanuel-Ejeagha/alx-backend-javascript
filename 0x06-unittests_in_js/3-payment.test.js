c6995e3c888901aeb9c4ed9bfb1432b6bad63f72
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-27 03:28:46 +0100

    task 3 test

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment.js');
+const sinon = require('sinon');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  beforeEach(function () {
+    Utils.calculateNumber = sinon.spy();
+  });
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('validates the Utils module method', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+  });
+
+  it('validates the Utils module method is called with argument', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.firstCall.args).to.deep.equal([
+      'SUM',
+      100,
+      20,
+    ]);
+  });
+});
